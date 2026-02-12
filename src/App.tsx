35c53ced07c7a097680d270525b13d3756efb878
Serhii bc2024-6 <tickhere135@gmail.com>
2026-02-12 20:34:52 +0200

    improve readability of conditional rendering using ternary operator

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-  import React from 'react';
+import React from 'react';
 
-  type State = {
-    pressedKey: string | null;
+type State = {
+  pressedKey: string | null;
+};
+
+export class App extends React.Component<{}, State> {
+  state: State = {
+    pressedKey: null,
+  };
+
+  handleKeyUp = (event: KeyboardEvent) => {
+    this.setState({ pressedKey: event.key });
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.handleKeyUp);
   }
 
-  export class App extends React.Component<{}, State> {
-    state: State = {
-      pressedKey: null,
-    };
-
-    handleKeyUp = (event: KeyboardEvent) => {
-      this.setState({ pressedKey: event.key });
-    };
-
-    componentDidMount() {
-      document.addEventListener('keyup', this.handleKeyUp);
-    }
-
-    componentWillUnmount() {
-      document.removeEventListener('keyup', this.handleKeyUp);
-    }
-
-    render() {
-      const { pressedKey } = this.state;
-
-      return (
-        <div className="App">
-          <p className="App__message">
-            {pressedKey && `The last pressed key is [${pressedKey}]` || `Nothing was pressed yet`}
-          </p>
-        </div>
-      );
-    }
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.handleKeyUp);
   }
+
+  render() {
+    const { pressedKey } = this.state;
+
+    return (
+      <div className="App">
+        <p className="App__message">
+          {pressedKey
+            ? `The last pressed key is [${pressedKey}]`
+            : 'Nothing was pressed yet'}
+        </p>
+      </div>
+    );
+  }
+}
